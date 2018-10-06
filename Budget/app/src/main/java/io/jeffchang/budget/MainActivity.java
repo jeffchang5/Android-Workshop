f690bac7553833c3c8af4fb95879b2e3d24edf75
Jeffrey Chang <me@jeffchang.io>
2018-10-06 01:46:35 -0700

    Added RecyclerView List for budget items

--- Budget/app/src/main/java/io/jeffchang/budget/MainActivity.java
+++ Budget/app/src/main/java/io/jeffchang/budget/MainActivity.java
@@ -3,6 +3,13 @@ package io.jeffchang.budget;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import io.jeffchang.budget.budgetlist.BudgetItem;
+import io.jeffchang.budget.budgetlist.BudgetListRecyclerViewAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView budgetRecyclerView = findViewById(R.id.activity_main_budget_recycler_view);
+
+
+        // Here we add mock data to test our UI
+        ArrayList<BudgetItem> budgetItemArrayList = new ArrayList<>();
+
+        BudgetItem gasItem = new BudgetItem(
+                "gas", "BP", "Utility", -43
+        );
+        budgetItemArrayList.add(gasItem);
+
+        BudgetItem tacoBellItem = new BudgetItem(
+                "taco_bell", "Taco Bell", "Food", -8
+        );
+        budgetItemArrayList.add(tacoBellItem);
+
+        BudgetItem payCheck = new BudgetItem(
+                "pay_check", "Pay Check", "Income", 3000
+        );
+        budgetItemArrayList.add(payCheck);
+
+
+        BudgetListRecyclerViewAdapter adapter = new BudgetListRecyclerViewAdapter(
+                budgetItemArrayList
+        );
+        budgetRecyclerView.setLayoutManager(
+                new LinearLayoutManager(this)
+        );
+        budgetRecyclerView.setAdapter(adapter);
     }
     
 }
---
+++ Budget/app/src/main/java/io/jeffchang/budget/budgetlist/BudgetListRecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package io.jeffchang.budget.budgetlist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.jeffchang.budget.R;
+
+public class BudgetListRecyclerViewAdapter extends RecyclerView.Adapter<
+        BudgetListRecyclerViewAdapter.BudgetListViewHolder> {
+
+    private ArrayList<BudgetItem> budgetItems;
+
+    public BudgetListRecyclerViewAdapter(ArrayList<BudgetItem> budgetItems) {
+        this.budgetItems = budgetItems;
+    }
+
+    @NonNull
+    @Override
+    public BudgetListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        View budgetItem = LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_budget, viewGroup, false);
+
+        BudgetListViewHolder viewHolder = new BudgetListViewHolder(budgetItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BudgetListViewHolder viewHolder, int i) {
+        viewHolder.bind(budgetItems.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return budgetItems.size();
+    }
+
+    class BudgetListViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView titleTextView;
+
+        private final TextView categoryTextView;
+
+        private final TextView amountTextView;
+
+        BudgetListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.item_budget_title_text_view);
+
+            categoryTextView = itemView.findViewById(R.id.item_budget_category_text_view);
+
+            amountTextView = itemView.findViewById(R.id.item_budget_amount_text_view);
+        }
+
+        public void bind(BudgetItem budgetItem) {
+            titleTextView.setText(budgetItem.getName());
+            categoryTextView.setText(budgetItem.getCategory());
+
+            String amount = Float.toString(
+                    budgetItem.getAmount()
+            );
+            amountTextView.setText(amount);
+        }
+    }
+}
